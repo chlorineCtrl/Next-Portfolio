320848a57194485ecdc0b7004aac30f4d04e435a
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-04-16 06:03:52 +0600

    intro component added

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowDown, BsLinkedin } from "react-icons/bs";
+import { BsArrowDown, BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 
@@ -32,6 +32,7 @@ export default function Intro() {
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] shadow-xl"
             />
           </motion.div>
+
           {/* emoji motion */}
           <motion.span
             className="absolute bottom-0 right-0 text-4xl"
@@ -48,6 +49,8 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+
+      {/* text motion */}
       <motion.p
         className="mb-10 px-4 mt-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
@@ -63,16 +66,25 @@ export default function Intro() {
           enjoy exploring <span className="font-bold">ideas for new apps.</span>
         </span>
       </motion.p>
-      <div>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
         <Link
           href="#contcat"
-          className="bg-gray-900 flex text-white px-7 py-3 items-center gap-2 rounded-full"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
         >
-          Contact me here <BsArrowDown />
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border borderBlack "
+          href="/Md-Abir-Hasan-Fuad-Resume.pdf"
           download
         >
           Download CV{" "}
@@ -80,21 +92,21 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack"
+          href="https://www.linkedin.com/in/muhammad-abir-0z7/"
           target="_blank"
         >
           <BsLinkedin />
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+          href="https://github.com/chlorineCtrl"
           target="_blank"
         >
           <FaGithubSquare />
         </a>
-      </div>
+      </motion.div>
     </section>
   );
 }
